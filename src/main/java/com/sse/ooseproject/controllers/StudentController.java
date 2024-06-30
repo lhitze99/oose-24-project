4e7ed55771fae9e97aa20cc46188629717d94c5d
smarx99 <stefanie.marx99@web.de>
2024-06-30 13:39:14 +0200

    Add Get endpoint student/new and student/edit

--- src/main/java/com/sse/ooseproject/controllers/StudentController.java
+++ src/main/java/com/sse/ooseproject/controllers/StudentController.java
@@ -2,6 +2,8 @@ package com.sse.ooseproject.controllers;
 
 import com.sse.ooseproject.StudentRepository;
 import com.sse.ooseproject.models.Student;
+import com.sse.ooseproject.models.Institute;
+import com.sse.ooseproject.InstituteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +17,12 @@ import java.util.stream.Collectors;
 public class StudentController {
 
     private final StudentRepository studentRepository;
+    private final InstituteRepository instituteRepository;
 
     @Autowired
-    public StudentController(StudentRepository studentRepository) {
+    public StudentController(StudentRepository studentRepository, InstituteRepository instituteRepository) {
         this.studentRepository = studentRepository;
-
+        this.instituteRepository = instituteRepository;
     }
 
     @GetMapping("/students")
@@ -61,4 +64,28 @@ public class StudentController {
         // Returning the name of a view (found in resources/templates) as a string lets this endpoint return that view.
         return "students";
     }
+
+    @GetMapping("/student/new")
+    public String newStudent(Model model) {
+        return setupStudentForm(null, model, "new");
+    }
+
+    @GetMapping("/student/edit")
+    public String editStudent(@RequestParam("id") Long id, Model model) {
+        return setupStudentForm(id, model, "edit");
+    }
+
+    private String setupStudentForm(Long id, Model model, String pageType) {
+        Student student = (id == null) ? new Student() : studentRepository.findById(id).orElse(null);
+        List<String> studySubjects = instituteRepository.findAll()
+                .stream()
+                .map(Institute::getProvidesStudySubject)
+                .collect(Collectors.toList());
+
+        model.addAttribute("student", student);
+        model.addAttribute("page_type", pageType);
+        model.addAttribute("study_subjects", studySubjects);
+        return "edit_student";
+    }
+
 }
